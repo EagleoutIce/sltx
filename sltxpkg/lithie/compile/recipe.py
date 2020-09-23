292d8a00f3b5ec9246f018dbf3ab31ae85323dbe
EagleoutIce <maeusepiep99@web.de>
2020-09-24 01:15:21 +0200

    failed resource try
    Signed-off-by: EagleoutIce <maeusepiep99@web.de>

--- sltxpkg/lithie/compile/recipe.py
+++ sltxpkg/lithie/compile/recipe.py
@@ -2,31 +2,30 @@
 class Recipe():
     
     settings = {
-        'name': 'Default recipe (latexmk!)',
-        'author': 'Florian Sihler',
+        'name': '',
+        'author': '',
         'use-cases': [],
         'hooks': {
             'pre': [],
             'post': []
         },
-        # Will add a hook to cleanup files
         'cleanup': True,
-        'args': '-pdf -file-line-error -halt-on-error -interaction=nonstopmode -outdir {working_dir}',
-        'eargs': ['-f'], # extra args
-        # Tools will be used to generate files if needed or to require intermediate steps
-        # They extend the hooks and can use ifs
-        # They will extend eargs as well (bib for example will add -bibtex?)
-        'tools': ['bibliography', 'glossary', 'index'],
+        'args': '',
+        'eargs': [],
+        'tools': [],
         'variables': {
-            # Variables will be used for expansion in other strings
             'working_dir': '{tmp}/sltx/compile',
             'filter': '{file}.pdf'
         },
-        # TODO: latexmk pretex
-        # TODO latexmkrc if no ile is needd?
         'command': 'latexmk {args} {eargs} {file}'
     }
 
-    def __init__(self):
+    def __init__(self, recipe_path : str):
         super().__init__()
         eval(self.settings['pre'])
+
+
+    @staticmethod
+    def get_default_recipes() -> [str]:
+        RECIPE_PATH = pkg_resources.resource_dir('')
+        pass # return [f for f in listdir()]
