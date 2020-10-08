8c2a7bf0de14777e4f8d919d0f9a8d700405f0a3
EagleoutIce <maeusepiep99@web.de>
2020-10-08 18:07:27 +0200

    refactoring: cleanup of the already refactored command-resolving
    Signed-off-by: EagleoutIce <maeusepiep99@web.de>

--- sltxpkg/command_config.py
+++ sltxpkg/command_config.py
@@ -1,4 +1,3 @@
-
 def add_argument_to_parser(p, *args, **kwargs):
     p.add_argument(*args, **kwargs)
 
@@ -16,7 +15,7 @@ class Arg:
 
 
 class Commands:
-    def __init__(self, cmds):
+    def __init__(self, cmds: dict):
         self.cmds = cmds
 
     def helper_values(self):
@@ -25,3 +24,18 @@ class Commands:
     def generate(self, p):
         for command in self.cmds.items():
             add_parser(p, command[0], command[1][2], **((dict(command[1][1].kwargs, aliases=command[1][0][1])) if len(command[1][0][1]) != 0 else command[1][1].kwargs))
+
+    @staticmethod
+    def __cmd_from_alias(alias: tuple) -> list:
+        return alias[1][0][1]
+
+    def __cmd_from_name(self, tc: str):
+        return self.cmds[tc][0][0]
+
+    def __find_in_alias(self, tc: str):
+        matching_commands = [alias[0] for alias in self.cmds.items() if tc in Commands.__cmd_from_alias(alias)]
+        # if multiple we take the first (should not happen)
+        return self.__cmd_from_name(matching_commands[0])
+
+    def get_cmd(self, search_key: str):
+        return self.__cmd_from_name(search_key) if search_key in self.cmds else self.__find_in_alias(search_key)
