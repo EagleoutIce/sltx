eaf9918be19110f18273def99fe5fb1d5e3ab90e
EagleoutIce <maeusepiep99@web.de>
2021-03-06 03:10:25 +0100

    further additions

--- sltxpkg/lithie/analyze/analyzer.py
+++ sltxpkg/lithie/analyze/analyzer.py
@@ -5,7 +5,7 @@ import os.path
 import re
 
 ANALYZER_PATTERN = re.compile(
-    '^! |LaTeX error|Undefined control sequence|improper alph|Incomplete \\\\if|Runaway preamble\\?|has an extra|Permission denied')
+    '^! |LaTeX error|Undefined control sequence|improper alph|Incomplete \\\\if|Runaway preamble\\?|has an extra|Permission denied|not loadable: Metric')
 
 
 class Analyzer():
