4e68cb71ba725343baa5a73f44fdf2274e6abf2a
EagleoutIce <maeusepiep99@web.de>
2021-03-07 14:27:38 +0100

    more errorcodes

--- sltxpkg/lithie/analyze/analyzer.py
+++ sltxpkg/lithie/analyze/analyzer.py
@@ -5,7 +5,7 @@ import os.path
 import re
 
 ANALYZER_PATTERN = re.compile(
-    '^! |Error:|Undefined control sequence|improper alph|Incomplete \\\\if|Runaway preamble\\?|has an extra|Permission denied|not loadable: Metric|Extra alignment tab has been|Can\'t create output')
+    '^! |Error:|Undefined control sequence|improper alph|Incomplete \\\\if|Runaway preamble\\?|has an extra|Permission denied|not loadable: Metric|Extra alignment tab has been|Can\'t create output|too long|Runaway argument|al parameter number')
 
 
 class Analyzer():
@@ -15,11 +15,12 @@ class Analyzer():
         self.idx = idx
 
     def analyze(self):
-        print("\033[34m"+self.file+"\033[m")
+        print("\033[34m"+self.file.replace(os.getcwd(), '\033[38;5;247m[cwd]\033[34m') +"\033[m")
         target = tempfile.TemporaryDirectory(prefix='sltx-ua-log').name
         shutil.unpack_archive(self.file, extract_dir=target)
         # TODO: make this more sensitive (makeindex logs etc...)
         templogs = glob.glob(os.path.join(target, '*.log'))
+        templogs += glob.glob(os.path.join(target, '*.ilg'))
         for templog in templogs:
             print("\033[34m  - "+templog+"\033[m")
             with open(templog) as tl:
@@ -32,4 +33,4 @@ class Analyzer():
                         print('    >', line, end='\033[m')
                         offsetpr -= 1
                         if offsetpr == 0:
-                            print("\033[38;5;239m    # -------------------------------------\033[m")
+                            print("\033[38;5;247m    # -------------------------------------\033[m")
