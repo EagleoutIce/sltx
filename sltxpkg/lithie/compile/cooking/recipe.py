75a36415d57ec29c465efa1f399d54fc18623d87
Florian Sihler <florian.sihler@uni-ulm.de>
2022-07-16 13:02:44 +0200

    logging replace actions

--- sltxpkg/lithie/compile/cooking/recipe.py
+++ sltxpkg/lithie/compile/cooking/recipe.py
@@ -51,10 +51,11 @@ class Recipe():
     def __init__(self, recipe_path: str, file: str, idx: str):
         super().__init__()
         self.file = file
+        self.idx = idx
         for file_end_pattern, file_end_replacer in sg.configuration[sg.C_FILE_END_REPLACERS].items():
             if file.endswith(file_end_pattern):
+                print_idx(self.idx, "Applying file name replace: " + file_end_pattern + " => " + file_end_replacer)
                 self.file = file[:-len(file_end_replacer)] + file_end_replacer
-        self.idx = idx
         recipe_full_path = recipe_path
         if not os.path.isfile(recipe_full_path):
             recipe_full_path = str(
